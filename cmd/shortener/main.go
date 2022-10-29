de4dc39f50b74eef382b4ca3f927fc2c60238345
Aleksandr Aleksandrov <87758457+alaleks@users.noreply.github.com>
2022-10-29 11:27:08 +0300

    добавлена проверка в main go ошибки при недоступности порта

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,6 +9,12 @@ import (
 
 func main() {
 	server, err := serv.New(":8080")
+	// здесь ошибка возвращается если недоступен порт
+	// поэтому нужна проверка перед стартом сервера
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	err = serv.Run(server)
 	if err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
