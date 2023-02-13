81dcb006a6447275a8f74b49007e5a89c703d6e3
Aleksandr Aleksandrov <87758457+alaleks@users.noreply.github.com>
2023-02-13 14:57:06 +0300

    Реализация икр. 19

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,13 +3,42 @@ package main
 
 import (
 	"errors"
+	"io"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
 
 	"github.com/alaleks/shortener/internal/app/serv"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
+func info() {
+	io.WriteString(os.Stdout, "Run application Shortener\n")
+
+	if len(buildVersion) != 0 {
+		io.WriteString(os.Stdout, "Build version: "+buildVersion+"\n")
+	} else {
+		io.WriteString(os.Stdout, "Build version: N/A\n")
+	}
+
+	if len(buildDate) != 0 {
+		io.WriteString(os.Stdout, "Build date: "+buildDate+"\n")
+	} else {
+		io.WriteString(os.Stdout, "Build date: N/A\n")
+	}
+
+	if len(buildCommit) != 0 {
+		io.WriteString(os.Stdout, "Build commit: "+buildCommit+"\n")
+	} else {
+		io.WriteString(os.Stdout, "Build commit: N/A\n")
+	}
+}
+
 func main() {
+	info()
 	server := serv.New()
 
 	// Run server for pprof
